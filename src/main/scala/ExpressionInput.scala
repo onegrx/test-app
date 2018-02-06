134b8b0ec0a31a03296117dad7991944189b8db2
onegrx <onegrx@gmail.com>
2018-02-06 02:06:57 +0100

    Omit trailing zeros in response JSON

--- src/main/scala/ExpressionInput.scala
+++
@@ -1,1 +0,0 @@
-final case class ExpressionInput(expression: String)
--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,15 +1,20 @@
+import java.math.RoundingMode
+import java.text.DecimalFormat
+
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import spray.json.RootJsonFormat
 import spray.json.DefaultJsonProtocol._
+import spray.json.{JsNumber, JsObject, JsValue, RootJsonFormat}
 
 import scala.concurrent.ExecutionContextExecutor
 import scala.io.StdIn
+
+final case class ExpressionInput(expression: String)
+final case class ExpressionOutput(result: Double)
 
 object Server {
 
@@ -20,12 +25,27 @@ object Server {
   def main(args: Array[String]): Unit = {
 
     implicit val inputFormat: RootJsonFormat[ExpressionInput] = jsonFormat1(ExpressionInput)
+    implicit object outputFormat extends RootJsonFormat[ExpressionOutput] {
+
+      val df = new DecimalFormat("###.#")
+      df.setRoundingMode(RoundingMode.HALF_UP)
+
+      override def write(obj: ExpressionOutput) = JsObject("result" -> JsNumber(df.format(obj.result)))
+      override def read(json: JsValue) = {
+        json.asJsObject.getFields("result") match {
+          case Seq(JsNumber(n)) => ExpressionOutput(n.toDouble)
+          case _ => spray.json.deserializationError("Can't read input")
+        }
+      }
+
+    }
 
     val route: Route =
       post {
         path("evaluate") {
           entity(as[ExpressionInput]) { expr =>
-            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, Calculator.calculate(expr.expression).toString))
+            val res = Calculator.calculate(expr.expression)
+            complete(ExpressionOutput(res))
           }
         }
       }
@@ -37,6 +57,5 @@ object Server {
       .flatMap(_.unbind())
       .onComplete(_ => system.terminate())
   }
-
 
 }
